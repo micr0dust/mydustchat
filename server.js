5921bde4731c20531fd9e379fa653836ae23c5ed
wuilliam104286 <wuilliamolgo104286@gmail.com>
2020-07-11 13:24:43 +0800

    ip request

--- server.js
+++ server.js
@@ -41,9 +41,10 @@ serv_io.set('log level', 1); // 關閉 debug 訊息
 let content = [];
 let userlist = [];
 let iplist = [];
-let ipnum=0;
-let news=false;
-const random = Math.floor(Math.random() * (2^64 - 2 + 1) + 2);
+let ipnum = 0;
+let news = false;
+let reapet = false;
+const random = Math.floor(Math.random() * (2 ^ 64 - 2 + 1) + 2);
 content.push({ "name": "伺服器", "text": "歡迎來到此聊天室!" });
 
 serv_io.sockets.on('connection', function (socket) {
@@ -52,28 +53,35 @@ serv_io.sockets.on('connection', function (socket) {
   }, 500);
   // 接收來自於瀏覽器的資料
   socket.on('client_data', function (data) {
-    let realuser=false;
-    for(i=0;i<iplist.length;i++) if(iplist[i]===data.ip) realuser=true;
-    if(data.text===""||!realuser||data.text.length>300) return console.log("阻止了一個不法請求");
-    news=true;
-    let txt=data.text;
-    let ip=data.ip.replace(/\./g,"");
+    let realuser = false;
+    let ip = data.ip.replace(/\./g, "");
+    for (i = 0; i < iplist.length+1; i++) if (iplist[i] === ip) realuser = true;
+    if (data.text === "" || !realuser || data.text.length > 300) return console.log("阻止了一個不法請求");
+    news = true;
+    let txt = data.text;
     content.push({ "name": userlist[ip], "text": txt });
     //if(content.length>50) content.shift();
     socket.emit('chat', { "chat": content, "user": false, "news": news });
     console.log({ "name": userlist[ip], "text": txt, "ip": ip })
-    news=false;
+    news = false;
   });
   socket.on('ip', function (data) {
-    for(i=0;i<iplist.length;i++) if(iplist[i]===data.ip) return;
-    let txt=data.ip;
-    iplist[ipnum]=txt;
+    if (!data.ip) return;
+    let txt = data.ip;
+    txt = txt.replace(/\./g, "");
+    for (i = 0; i < iplist.length; i++) if (iplist[i] === txt) reapet=true;
+    if(reapet){
+      socket.emit('chat', { "chat": content, "user": userlist[txt] });
+      reapet=false;
+      console.log("A");
+      return;
+    }
+    iplist[ipnum] = txt;
     ipnum++;
-    txt=txt.replace(/\./g,"");
-    userlist[txt]=(parseInt(txt)+random).toString(35);
-    console.log(userlist[txt]+"加入了聊天室");
-    content.push({ "name": "伺服器", "text": userlist[txt]+"加入了聊天室" });
-    socket.emit('chat', { "chat": content, "user": userlist[txt] , "news": news });
+    userlist[txt] = (parseInt(txt) + random).toString(35);
+    console.log(userlist[txt] + "加入了聊天室");
+    content.push({ "name": "伺服器", "text": userlist[txt] + "加入了聊天室" });
+    socket.emit('chat', { "chat": content, "user": userlist[txt], "news": news });
     //if(content.length>50) content.shift();
   });
 });
