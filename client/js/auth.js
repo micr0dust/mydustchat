749f7965a9130e5271a82f1a5550fa046f935409
microdust <wuilliamolgo104286@gmail.com>
2021-09-12 19:32:33 +0800

    app connect

---
+++ client/js/auth.js
@@ -0,0 +1,10 @@
+var authData = {
+    user: function(info) {
+        return "microdust"
+    },
+    password: function(warning) {
+        return "ux7e4ywp"
+    }
+};
+
+module.exports = authData;
